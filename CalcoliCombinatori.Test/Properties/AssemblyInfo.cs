ed70bbcda4e558f023e9045255a18cb51205c7bb
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-24 13:25:41 +0100

    Add project files.

---
+++ CalcoliCombinatori.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcoliCombinatori.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcoliCombinatori.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a199b43-d1d6-45ed-902c-266bdf553df1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EquaioniLibrary/Equazioni.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EquaioniLibrary
+{
+    public class Equazioni
+    {
+            private static int Fattoriale(int n)
+            {
+                if (n < 0)
+            {
+                return 0;
+            }
+                else if (n <= 1)
+            {
+                return 1;
+            }   
+                else
+                {
+                    int prodotto = 1;
+                    for (int i = 2; i <= n; i++)
+                    {
+                        prodotto *= i;
+                    }
+                    return prodotto;
+                }
+            }
+    }
+}
